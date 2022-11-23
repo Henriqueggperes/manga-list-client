14a082ce42364f6821d07e8d8e371fc69c355085
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-11-23 11:21:30 -0300

    feat: initial resposivity

--- manga-list-front/src/pages/login/login.tsx
+++ manga-list-front/src/pages/login/login.tsx
@@ -1,51 +1,35 @@
-import React from 'react';
-import logo from './../../assets/images/Logo.png';
+import React from "react";
+import logo from "./../../assets/images/Logo.png";
 import "./login.css";
 import { AiOutlineHome } from "react-icons/ai";
 
 const Login = () => {
   return (
+    <div id="divContainerLogin">
+      <div className="returnHome">
+        <AiOutlineHome id="homeButton"></AiOutlineHome>
+      </div>
+      <div className="loginLogoContainer">
+        <img src={logo}></img>
+      </div>
+      <div className="loginFormContainer">
 
-    <div id='divContainerLogin'>
-
-        <div className='returnHome'>
-            <AiOutlineHome id='homeButton'></AiOutlineHome>
-        </div>
-       
-       <div id='divLogo'>
-            <img src={logo} className="logo" alt="Logo" />
-       </div>
-
-        <form id='formUserData'>
-            <div id="divUsernameLogin">
-                <label className='labelLogin' id="labelUsernameLogin" htmlFor="usernameLogin">Username</label>
-                <input className='loginFormInput' id="usernameLogin" type="text" />
-            </div>
-
-            <div id="divPasswordLogin">
-                <label className='labelLogin' id='labelPasswordLogin' htmlFor="usernameLogin">Password</label>
-                <input className='loginFormInput' id='passwordLogin' type="text" />
-            </div>
-
-            <div id='divButtonLogin'>
-                <button id='buttonLogin' className='buttonDefault'>ENTRAR</button>
-            </div>
+        <form className="loginForm">
+          <div className="loginFormInputsContainer">
+            <input required name="e-mail" type="email" />
+            <label htmlFor="e-mail">E-Mail</label>
+          </div>
+          <div className="loginFormInputsContainer">
+            <input required name="password" type="password" />
+            <label htmlFor="password">Password</label>
+          </div>
+          <div id="loginFormSendButtonContainer">
+             <button>ENVIAR</button>
+          </div>
         </form>
-
-        <div id='divRegister'>
-            <span id='spanRegister'>Não tem uma conta?</span>
-            <button id='registerButton' className='buttonDefault'>CADASTRE-SE</button>
-
-        </div>
-
-        
-
-
-
+      </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Login
+export default Login;
